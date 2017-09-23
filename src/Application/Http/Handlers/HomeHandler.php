99e5f92b4f08e602212482a3abc54e9f0e57d147
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-09-23 23:05:59 +0200

    :sparkles: Use DI with Request & Response in http handlers

--- src/Application/Http/Handlers/HomeHandler.php
+++ src/Application/Http/Handlers/HomeHandler.php
@@ -2,13 +2,22 @@
 
 namespace Wouterds\Application\Http\Handlers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 class HomeHandler
 {
     /**
      * Home request handler
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return Response
      */
-    public function __invoke()
+    public function __invoke(Request $request, Response $response): Response
     {
-        echo 'Hello World!';
+        $response->getBody()->write('Hello World!');
+
+        return $response;
     }
 }
