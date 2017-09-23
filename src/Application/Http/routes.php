af553cb831448d6f536ea36efea46d959c4c62f2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-09-24 00:10:50 +0200

    :bulb: Extra documentation for PHPStorm type-hinting

--- src/Application/Http/routes.php
+++ src/Application/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use Wouterds\Application\Http\Application;
 use Wouterds\Application\Http\Handlers\HomeHandler;
+
+/** @var Application $app */
 
 $app->get('/', HomeHandler::class)->setName('home');
