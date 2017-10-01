bcbf5565cca75a28c18dc3ed99f0def3f0a81893
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-01 20:17:22 +0200

    :zap: Load TwigMiddleware for all regular view routes

--- src/Application/Http/routes.php
+++ src/Application/Http/routes.php
@@ -2,7 +2,11 @@
 
 use Wouterds\Application\Http\Application;
 use Wouterds\Application\Http\Handlers\HomeHandler;
+use Wouterds\Application\Http\Middlewares\TwigMiddleware;
 
 /** @var Application $app */
 
-$app->get('/', HomeHandler::class)->setName('home');
+// Routes that'll render Twig templates
+$app->group(null, function () use ($app) {
+    $app->get('/', HomeHandler::class)->setName('home');
+})->add(TwigMiddleware::class);
