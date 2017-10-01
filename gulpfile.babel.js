f991e6592a216a08f59dff630ba06dac3d5963a8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-01 09:33:14 +0200

    :wrench: Basic Gulp styles task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,17 @@
 import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
 
 const folder = {
   dist: {
-    styles: '/public/css',
-    scripts: '/public/js',
+    styles: './public/css',
+    scripts: './public/js',
   },
   resources: {
-    styles: '/resources/styles',
-    scripts: '/resources/scripts',
+    styles: './resources/styles',
+    scripts: './resources/scripts',
   },
 };
 
@@ -19,7 +23,23 @@ class TaskRunner {
   }
 
   styles() {
-    console.log('styles');
+    return gulp
+    .src(folder.resources.styles + '/**.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }).on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          'last 3 versions',
+          'last 2 major versions',
+          'ie >= 9',
+        ],
+      }),
+    ]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(folder.dist.styles))
   }
 
   scripts() {
