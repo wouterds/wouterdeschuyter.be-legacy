2af0d3b38e06fd47d449e317497e8e8c60cc90f3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-01 21:34:00 +0200

    :sparkles: Added User object

---
+++ src/Domain/Users/User.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Wouterds\Domain\Users;
+
+use Carbon\Carbon;
+
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var boolean
+     */
+    private $approved;
+
+    /**
+     * @var Carbon
+     */
+    private $createdAt;
+
+    /**
+     * @var Carbon|null
+     */
+    private $updatedAt;
+
+    /**
+     * @var Carbon|null
+     */
+    private $deletedAt;
+
+    /**
+     * @param string $name
+     * @param string $email
+     * @param bool $approved
+     */
+    public function __construct(string $name, string $email, bool $approved)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->approved = $approved;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isApproved(): bool
+    {
+        return $this->approved;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getCreatedAt(): Carbon
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return Carbon|null
+     */
+    public function getUpdatedAt(): ?Carbon
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @return Carbon|null
+     */
+    public function getDeletedAt(): ?Carbon
+    {
+        return $this->deletedAt;
+    }
+}
