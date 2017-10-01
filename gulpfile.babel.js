fdbc387bb84142920506fd832a136b11b2ba5c51
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-01 10:22:38 +0200

    :bug: Forgot a semicolon

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,7 +25,7 @@ class TaskRunner {
   constructor() {
     gulp.task('styles', this.styles);
     gulp.task('scripts', this.scripts);
-    gulp.task('default', ['styles', 'scripts'])
+    gulp.task('default', ['styles', 'scripts']);
   }
 
   styles() {
