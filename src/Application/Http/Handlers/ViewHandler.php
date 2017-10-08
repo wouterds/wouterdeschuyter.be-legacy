3cb51672b53cbf90ac517a634fcfcaa247fdc44a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-08 15:19:09 +0200

    :sparkles: Share some page info with Twig

--- src/Application/Http/Handlers/ViewHandler.php
+++ src/Application/Http/Handlers/ViewHandler.php
@@ -30,6 +30,9 @@ abstract class ViewHandler implements View
      */
     public function render(Response $response, array $data = []): Response
     {
+        // Add extra page-info
+        $data['pageInfo'] = $this->pageInfo();
+
         // Render template to response
         return $this->renderer->renderWithResponse($response, $this->getTemplate(), $data);
     }
@@ -44,5 +47,27 @@ abstract class ViewHandler implements View
     public function __invoke(Request $request, Response $response): Response
     {
         return $this->render($response);
+    }
+
+    /**
+     * @return array
+     */
+    private function pageInfo(): array
+    {
+        $templateName = $this->getTemplate();
+        $templateName = str_replace('.html.twig', null, $templateName);
+        $templateParts = preg_split( "/(-|\/)/", $templateName );
+        $templateParts = array_filter($templateParts);
+
+        $pascalCaseName = array_map('ucfirst', $templateParts);
+        $pascalCaseName = implode('', $pascalCaseName);
+
+        $dashedCaseName = implode('-', $templateParts);
+        $dashedCaseName = lcfirst($dashedCaseName);
+
+        return [
+            'pascalCase' => $pascalCaseName,
+            'dashedCase' => $dashedCaseName,
+        ];
     }
 }
