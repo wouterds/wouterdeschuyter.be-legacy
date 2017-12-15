ea19581c5a9058c10eb48b784630c3d5162cc28e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 15:29:21 +0100

    :sparkles: Add support for running PHPUnit database tests

--- src/Infrastructure/Database/ServiceProvider.php
+++ src/Infrastructure/Database/ServiceProvider.php
@@ -21,14 +21,26 @@ class ServiceProvider extends AbstractServiceProvider
     public function register()
     {
         $this->container->share(Connection::class, function () {
+            $dbName = getenv('MYSQL_DATABASE');
+            $user = getenv('MYSQL_USER');
+            $password = getenv('MYSQL_PASSWORD');
+            $host = getenv('MYSQL_HOST');
+            $driver = 'pdo_mysql';
+            $charset = 'utf8mb4';
+            $collate = 'utf8mb4_unicode_ci';
+
+            if (defined('PHPUNIT')) {
+                $dbName = getenv('MYSQL_TEST_DATABASE');
+            }
+
             return DriverManager::getConnection([
-                'dbname' => getenv('MYSQL_DATABASE'),
-                'user' => getenv('MYSQL_USER'),
-                'password' => getenv('MYSQL_PASSWORD'),
-                'host' => getenv('MYSQL_HOST'),
-                'driver' => 'pdo_mysql',
-                'charset' => 'utf8mb4',
-                'collate' => 'utf8mb4_unicode_ci',
+                'dbname' => $dbName,
+                'user' => $user,
+                'password' => $password,
+                'host' => $host,
+                'driver' => $driver,
+                'charset' => $charset,
+                'collate' => $collate,
             ]);
         });
     }
