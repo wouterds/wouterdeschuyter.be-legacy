59e705163491db2fa2d9755ae3a30482869b56f4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 13:03:21 +0100

    :bulb: Forgot to typehint $name

--- src/Infrastructure/View/Twig.php
+++ src/Infrastructure/View/Twig.php
@@ -9,11 +9,11 @@ class Twig extends Twig_Environment
 {
     /**
      * @param Response $response
-     * @param $name
+     * @param string $name
      * @param array $context
      * @return Response
      */
-    public function renderWithResponse(Response $response, $name, array $context = []): Response
+    public function renderWithResponse(Response $response, string $name, array $context = []): Response
     {
         // Render template
         $contents = $this->render($name, $context);
