8667ff215b7b302563f3a32e07568fbefd81e158
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 18:59:21 +0100

    :sparkles: Added send method to Mailer interface

--- src/Infrastructure/Mail/Mailer.php
+++ src/Infrastructure/Mail/Mailer.php
@@ -2,6 +2,13 @@
 
 namespace WouterDeSchuyter\Infrastructure\Mail;
 
+use WouterDeSchuyter\Infrastructure\Mail\Message\Message;
+
 interface Mailer
 {
+    /**
+     * @param Message $mail
+     * @return bool
+     */
+    public function send(Message $mail): bool;
 }
