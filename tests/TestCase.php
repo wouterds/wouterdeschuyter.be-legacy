8ede33fdbd9f525e943d45a330b525e34d63315f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 16:17:41 +0100

    :sparkles: Added randomUser method to abatract TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace WouterDeSchuyter\Tests;
 use Faker\Factory;
 use Faker\Generator;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use WouterDeSchuyter\Domain\Users\User;
 
 abstract class TestCase extends PHPUnitTestCase
 {
@@ -19,5 +20,17 @@ abstract class TestCase extends PHPUnitTestCase
     public function bootstrapFaker()
     {
         $this->faker = Factory::create();
+    }
+
+    /**
+     * @return User
+     */
+    protected function randomUser()
+    {
+        return User::fromArray([
+            'email' => $this->faker->email,
+            'password' => $this->faker->sha256,
+            'name' => $this->faker->name,
+        ]);
     }
 }
