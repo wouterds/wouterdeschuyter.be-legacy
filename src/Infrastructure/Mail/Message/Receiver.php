26aa4033564f72179105cec168bbd2669d74fb8c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 18:55:59 +0100

    :sparkles: Added Mail/Message/Receiver object

---
+++ src/Infrastructure/Mail/Message/Receiver.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WouterDeSchuyter\Infrastructure\Mail\Message;
+
+class Receiver
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @param string $name
+     * @param string $email
+     */
+    public function __construct(string $name, string $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+}
