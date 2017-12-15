fb7cf1cf25336a2b07663048b617442cf5edae49
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 20:59:19 +0100

    :sparkles: ContactEnquiry command handler

---
+++ src/Application/Commands/ContactEnquiryHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WouterDeSchuyter\Application\Commands;
+
+use WouterDeSchuyter\Domain\Commands\ContactEnquiry;
+
+class ContactEnquiryHandler
+{
+    /**
+     * @param ContactEnquiry $contactEnquiry
+     */
+    public function handle(ContactEnquiry $contactEnquiry)
+    {
+    }
+}
