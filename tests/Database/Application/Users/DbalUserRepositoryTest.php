46c0401c917bf2a864f3353252b8978f947a6a27
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-15 16:17:59 +0100

    :white_check_mark: Added DbalUserRepositoryTest

---
+++ tests/Database/Application/Users/DbalUserRepositoryTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace WouterDeSchuyter\Tests\Database\Application\Users;
+
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use WouterDeSchuyter\Application\Users\DbalUserRepository;
+use WouterDeSchuyter\Domain\Users\UserId;
+use WouterDeSchuyter\Domain\Users\UserRepository;
+use WouterDeSchuyter\Tests\Database\DatabaseTestCase;
+
+class DbalUserRepositoryTest extends DatabaseTestCase
+{
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    public function setUp()
+    {
+        $this->repository = new DbalUserRepository($this->getConnection());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAddNewUser()
+    {
+        $user = $this->randomUser();
+        $this->repository->add($user);
+
+        $this->assertRowExists(DbalUserRepository::TABLE, [
+            'id' => $user->getId(),
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowWhenTryingToAddSameUserAgain()
+    {
+        $user = $this->randomUser();
+        $this->repository->add($user);
+
+        $this->expectException(UniqueConstraintViolationException::class);
+        $this->repository->add($user);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFindExistingUserById()
+    {
+        $user = $this->randomUser();
+        $this->repository->add($user);
+
+        $found = $this->repository->find($user->getId());
+
+        $this->assertEquals($user, $found);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnNullWhenTryingToFindNotExistingUserById()
+    {
+        $user = $this->randomUser();
+        $this->repository->add($user);
+
+        $found = $this->repository->find(new UserId($this->faker->uuid));
+
+        $this->assertNull($found);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFindExistingUserByEmail()
+    {
+        $user = $this->randomUser();
+        $this->repository->add($user);
+
+        $found = $this->repository->findByEmail($user->getEmail());
+
+        $this->assertEquals($user, $found);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnNullWhenTryingToFindNotExistingUserByEmail()
+    {
+        $user = $this->randomUser();
+        $this->repository->add($user);
+
+        $found = $this->repository->findByEmail($this->faker->email);
+
+        $this->assertNull($found);
+    }
+}
