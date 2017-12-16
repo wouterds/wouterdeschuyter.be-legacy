c95b0604ad730051c2eaa5943b16275186466ce6
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-16 10:42:44 +0100

    :sparkles: Send mail in ContactEnquiry handler

--- src/Application/Commands/ContactEnquiryHandler.php
+++ src/Application/Commands/ContactEnquiryHandler.php
@@ -3,13 +3,36 @@
 namespace WouterDeSchuyter\Application\Commands;
 
 use WouterDeSchuyter\Domain\Commands\ContactEnquiry;
+use WouterDeSchuyter\Infrastructure\Mail\Mailer;
+use WouterDeSchuyter\Infrastructure\Mail\Message\Factory as MessageFactory;
+use WouterDeSchuyter\Infrastructure\Mail\Message\Sender;
 
 class ContactEnquiryHandler
 {
+    /**
+     * @var Mailer
+     */
+    private $mailer;
+
+    /**
+     * @param Mailer $mailer
+     */
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     /**
      * @param ContactEnquiry $contactEnquiry
      */
     public function handle(ContactEnquiry $contactEnquiry)
     {
+        $message = MessageFactory::startWithDefault()
+            ->withSender(new Sender($contactEnquiry->getName(), $contactEnquiry->getEmail()))
+            ->withSubject($contactEnquiry->getSubject())
+            ->withBody($contactEnquiry->getMessage())
+            ->build();
+
+        $this->mailer->send($message);
     }
 }
