8ef5dc30dd426b18587618dac15bb1746a5072be
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-18 22:28:14 +0100

    :card_file_box: Added media migration

---
+++ database/migrations/Version20171218212159.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+
+namespace WouterDeSchuyter\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171218212159 extends AbstractMigration
+{
+    private const TABLE = 'media';
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable(self::TABLE);
+        $table->addColumn('id', 'uuid');
+        $table->addColumn('user_id', 'uuid');
+        $table->addColumn('name', 'string')->setLength(64);
+        $table->addColumn('content_type', 'string')->setLength(32);
+        $table->addColumn('size', 'integer');
+        $table->addColumn('md5', 'string')->setLength(32);
+        $table->addColumn('created_at', 'datetime')->setDefault('CURRENT_TIMESTAMP');
+        $table->addColumn('updated_at', 'datetime')->setNotnull(false);
+        $table->addColumn('deleted_at', 'datetime')->setNotnull(false);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['user_id', 'md5']);
+        $table->addIndex(['deleted_at']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable(self::TABLE);
+    }
+}
