e388d9c81ab212dfb58fb9264f009cb10855bdf2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-19 16:18:25 +0100

    :sparkles: Added DATETIME const

--- src/Infrastructure/ValueObjects/DateTime.php
+++ src/Infrastructure/ValueObjects/DateTime.php
@@ -4,11 +4,13 @@ namespace WouterDeSchuyter\Infrastructure\ValueObjects;
 
 class DateTime extends \DateTime
 {
+    const DATETIME = 'Y-m-d H:i:s';
+
     /**
      * @return string
      */
     public function __toString()
     {
-        return self::format(self::ATOM);
+        return self::format(self::DATETIME);
     }
 }
