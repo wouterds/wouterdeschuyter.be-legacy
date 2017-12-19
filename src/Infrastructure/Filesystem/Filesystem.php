cdf9b2daaac5592c980399b3cacddc33469c89c4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-19 10:22:57 +0100

    :sparkles: Added Filesystem

---
+++ src/Infrastructure/Filesystem/Filesystem.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace WouterDeSchuyter\Infrastructure\Filesystem;
+
+use Exception;
+use League\Flysystem\Filesystem as LeagueFilesystem;
+use Psr\Http\Message\StreamInterface;
+use Slim\Http\Stream;
+use WouterDeSchuyter\Domain\Media\Media;
+use WouterDeSchuyter\Domain\Media\MediaRepository;
+
+class Filesystem
+{
+    /**
+     * @var MediaRepository
+     */
+    private $mediaRepository;
+
+    /**
+     * @var LeagueFilesystem
+     */
+    private $filesystem;
+
+    /**
+     * @param LeagueFilesystem $filesystem
+     * @param MediaRepository $mediaRepository
+     */
+    public function __construct(LeagueFilesystem $filesystem, MediaRepository $mediaRepository)
+    {
+        $this->filesystem = $filesystem;
+        $this->mediaRepository = $mediaRepository;
+    }
+
+    /**
+     * @param Media $media
+     * @param StreamInterface $stream
+     * @return bool
+     */
+    public function store(Media $media, StreamInterface $stream): bool
+    {
+        if ($this->filesystem->putStream($media->getPath(), $stream->detach()) === false) {
+            return false;
+        }
+
+        $media->setMd5($this->filesystem->hash($media->getPath(), 'md5'));
+
+        try {
+            $this->mediaRepository->add($media);
+        } catch (Exception $e) {
+            $this->filesystem->delete($media->getPath());
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param Media $media
+     * @return StreamInterface
+     */
+    public function get(Media $media): StreamInterface
+    {
+        return new Stream($this->filesystem->readStream($media->getPath()));
+    }
+}
---
+++ src/Infrastructure/Filesystem/ServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WouterDeSchuyter\Infrastructure\Filesystem;
+
+use Emgag\Flysystem\Hash\HashPlugin;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local as LocalAdapter;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        Filesystem::class,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        $this->container->share(Filesystem::class, function () {
+            $localAdapter = new LocalAdapter(APP_DIR . getenv('FILESYSTEM_DIR'));
+
+            $filesystem = new Filesystem($localAdapter);
+            $filesystem->addPlugin(new HashPlugin());
+
+            return $filesystem;
+        });
+    }
+}
