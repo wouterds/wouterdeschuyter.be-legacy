6636dd80f6c880a916c3ea87533cf43f534186dc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-21 18:28:35 +0100

    :construction: BlogPost Add WIP - Load media & users selection

--- src/Application/Http/Handlers/Admin/Blog/AddHandler.php
+++ src/Application/Http/Handlers/Admin/Blog/AddHandler.php
@@ -4,10 +4,42 @@ namespace WouterDeSchuyter\Application\Http\Handlers\Admin\Blog;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Router;
 use WouterDeSchuyter\Application\Http\Handlers\Admin\ViewHandler;
+use WouterDeSchuyter\Domain\Media\MediaRepository;
+use WouterDeSchuyter\Domain\Users\AuthenticatedUser;
+use WouterDeSchuyter\Domain\Users\UserRepository;
+use WouterDeSchuyter\Infrastructure\ApplicationMonitor\ApplicationMonitor;
+use WouterDeSchuyter\Infrastructure\Config\Config;
+use WouterDeSchuyter\Infrastructure\View\Twig;
 
 class AddHandler extends ViewHandler
 {
+    /**
+     * @var MediaRepository
+     */
+    private $mediaRepository;
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(
+        Twig $twig,
+        Config $config,
+        Router $router,
+        Request $request,
+        ApplicationMonitor $applicationMonitor,
+        AuthenticatedUser $authenticatedUser,
+        MediaRepository $mediaRepository,
+        UserRepository $userRepository
+    ) {
+        parent::__construct($twig, $config, $router, $request, $applicationMonitor, $authenticatedUser);
+
+        $this->mediaRepository = $mediaRepository;
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @return string
      */
@@ -23,6 +55,13 @@ class AddHandler extends ViewHandler
      */
     public function __invoke(Request $request, Response $response): Response
     {
-        return $this->render($response);
+        $media = $this->mediaRepository->findAll();
+        $users = $this->userRepository->findAll();
+
+        $data = [];
+        $data['media'] = $media;
+        $data['users'] = $users;
+
+        return $this->render($response, $data);
     }
 }
