1c24841e210641f57a53c5a574ff3aa54ae734b6
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-25 00:48:51 +0100

    :hankey: Twemoji

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+import Twemoji from 'twemoji';
 import PageAbout from './pages/about';
 import PageContact from './pages/contact';
 import PageAdminUsersSignIn from './pages/admin/users/sign-in';
@@ -12,6 +14,7 @@ class App {
 
   init() {
     this.initPages();
+    this.initTwemoji();
   }
 
   initPages() {
@@ -21,6 +24,23 @@ class App {
     new PageAdminUsersSignUp();
     new PageAdminMediaAdd();
     new PageAdminBlogPost();
+  }
+
+  initTwemoji() {
+    Twemoji.parse($('main')[0], {
+      folder: 'svg',
+      ext: '.svg',
+      callback: function (icon, options) {
+        switch (icon) {
+          case 'a9': // © copyright
+          case 'ae': // ® registered trademark
+          case '2122': // ™ trademark
+            return false;
+        }
+
+        return ''.concat(options.base, options.size, '/', icon, options.ext);
+      }
+    });
   }
 }
 
