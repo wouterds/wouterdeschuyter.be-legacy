47020718f0214ffdd23bbff2165685ffc2ea9333
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-12-25 10:52:10 +0100

    :sparkles: Also show blogposts on homepage

--- src/Application/Http/Handlers/HomeHandler.php
+++ src/Application/Http/Handlers/HomeHandler.php
@@ -2,6 +2,9 @@
 
 namespace WouterDeSchuyter\Application\Http\Handlers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+use WouterDeSchuyter\Domain\Blog\BlogPostRepository;
 use WouterDeSchuyter\Infrastructure\View\ViewAwareInterface;
 use WouterDeSchuyter\Infrastructure\View\ViewAwareTrait;
 
@@ -10,10 +13,38 @@ class HomeHandler implements ViewAwareInterface
     use ViewAwareTrait;
 
     /**
+     * @var BlogPostRepository
+     */
+    private $blogPostRepository;
+
+    /**
+     * @param BlogPostRepository $blogPostRepository
+     */
+    public function __construct(BlogPostRepository $blogPostRepository)
+    {
+        $this->blogPostRepository = $blogPostRepository;
+    }
+
+    /**
      * @return string
      */
     public function getTemplate(): string
     {
         return 'pages/home.html.twig';
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $blogPosts = $this->blogPostRepository->findAll();
+
+        $data = [];
+        $data['blogPosts'] = $blogPosts;
+
+        return $this->render($response, $data);
     }
 }
