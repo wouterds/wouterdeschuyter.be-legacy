8c911709bc73adf279444b286c223228ee049c7a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-01-03 21:57:17 +0200

    :sparkles: Added url to AddMedia command

--- src/Domain/Commands/Media/AddMedia.php
+++ src/Domain/Commands/Media/AddMedia.php
@@ -12,32 +12,47 @@ class AddMedia
     private $label;
 
     /**
+     * @var string
+     */
+    private $url;
+
+    /**
      * @var UploadedFile
      */
     private $uploadedFile;
 
     /**
      * @param string|null $label
-     * @param UploadedFile $uploadedFile
+     * @param string|null $url
+     * @param UploadedFile|null $uploadedFile
      */
-    public function __construct(string $label = null, UploadedFile $uploadedFile)
+    public function __construct(string $label = null, string $url = null, UploadedFile $uploadedFile = null)
     {
         $this->label = $label;
+        $this->url = $url;
         $this->uploadedFile = $uploadedFile;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getLabel(): string
+    public function getLabel(): ?string
     {
         return $this->label;
     }
 
     /**
-     * @return UploadedFile
+     * @return string|null
      */
-    public function getUploadedFile(): UploadedFile
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return UploadedFile|null
+     */
+    public function getUploadedFile(): ?UploadedFile
     {
         return $this->uploadedFile;
     }
