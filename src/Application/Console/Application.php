0304d56e9e0f6fea6b9644208904f47e7033ca2e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-01-15 12:49:30 +0100

    :sparkles: Added console application

---
+++ src/Application/Console/Application.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WouterDeSchuyter\Application\Console;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Command\Command;
+use WouterDeSchuyter\Application\Container;
+
+class Application extends SymfonyApplication
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * @param string $name
+     * @param string $version
+     */
+    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN')
+    {
+        parent::__construct($name, $version);
+
+        $this->container = Container::load();
+    }
+
+    /**
+     * @param string $name
+     * @param string $class
+     */
+    public function addCommand(string $name, string $class)
+    {
+        /** @var Command $command */
+        $command = $this->container->get($class);
+        $command->setName($name);
+
+        $this->add($command);
+    }
+}
