fb2a2410dd653260e506008fdce9385914a56e5c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-01-21 02:27:41 +0100

    :rotating_light: Linting™

--- src/Application/Console/Commands/GenerateRobots.php
+++ src/Application/Console/Commands/GenerateRobots.php
@@ -74,19 +74,33 @@ class GenerateRobots extends Command
     private function getAsciHeader(): string
     {
         $line = '';
-        $line .= '#                      _               _                _                 _            _' . PHP_EOL;
-        $line .= '#                     | |             | |              | |               | |          | |' . PHP_EOL;
-        $line .= '# __      _____  _   _| |_ ___ _ __ __| | ___  ___  ___| |__  _   _ _   _| |_ ___ _ __| |__   ___' . PHP_EOL;
-        $line .= '# \ \ /\ / / _ \| | | | __/ _ \ \'__/ _` |/ _ \/ __|/ __| \'_ \| | | | | | | __/ _ \ \'__| \'_ \ / _ \\' . PHP_EOL;
-        $line .= '#  \ V  V / (_) | |_| | ||  __/ | | (_| |  __/\__ \ (__| | | | |_| | |_| | ||  __/ | _| |_) |  __/' . PHP_EOL;
-        $line .= '#   \_/\_/ \___/ \__,_|\__\___|_|  \__,_|\___||___/\___|_| |_|\__,_|\__, |\__\___|_|(_)_.__/ \___|' . PHP_EOL;
-        $line .= '#                                               __/ |' . PHP_EOL;
-        $line .= '#                                              |___/' . PHP_EOL;
-        $line .= '#' . PHP_EOL;
-        $line .= '# v' . $this->config->get('APP_VERSION_NUMBER') . ' - ' . $this->config->get('APP_VERSION_COMMIT') . PHP_EOL;
-        $line .= '#' . PHP_EOL;
-        $line .= '# robots.txt' . PHP_EOL;
-        $line .= '#' . PHP_EOL;
+        $line .= '#                      _               _                _                 _            _';
+        $line .= PHP_EOL;
+        $line .= '#                     | |             | |              | |               | |          | |';
+        $line .= PHP_EOL;
+        $line .= '# __      _____  _   _| |_ ___ _ __ __| | ___  ___  ___| |__  _   _ _   _| |_ ___ _ __| |__   ___';
+        $line .= PHP_EOL;
+        $line .= '# \ \ /\ / / _ \| | | | __/ _ \ \'__/ _` |/ _ \/ __|/ __| \'_ \| | | | | | | __/ _ \ \'__| \'_ \ / _';
+        $line .= ' \\';
+        $line .= PHP_EOL;
+        $line .= '#  \ V  V / (_) | |_| | ||  __/ | | (_| |  __/\__ \ (__| | | | |_| | |_| | ||  __/ | _| |_) |  __/';
+        $line .= PHP_EOL;
+        $line .= '#   \_/\_/ \___/ \__,_|\__\___|_|  \__,_|\___||___/\___|_| |_|\__,_|\__, |\__\___|_|(_)_.__/ \___|';
+        $line .= PHP_EOL;
+        $line .= '#                                               __/ |';
+        $line .= PHP_EOL;
+        $line .= '#                                              |___/';
+        $line .= PHP_EOL;
+        $line .= '#';
+        $line .= PHP_EOL;
+        $line .= '# v' . $this->config->get('APP_VERSION_NUMBER') . ' - ' . $this->config->get('APP_VERSION_COMMIT');
+        $line .= PHP_EOL;
+        $line .= '#';
+        $line .= PHP_EOL;
+        $line .= '# robots.txt';
+        $line .= PHP_EOL;
+        $line .= '#';
+        $line .= PHP_EOL;
 
         return $line;
     }
