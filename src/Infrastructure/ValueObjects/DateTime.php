959a6779410e0b83e2e43863301544197dba652b
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-01-27 23:56:31 +0000

    :sparkles: Added fromTimestamp

--- src/Infrastructure/ValueObjects/DateTime.php
+++ src/Infrastructure/ValueObjects/DateTime.php
@@ -7,6 +7,15 @@ class DateTime extends \DateTime
     const DATETIME = 'Y-m-d H:i:s';
 
     /**
+     * @param int $timestamp
+     * @return DateTime
+     */
+    public static function fromTimestamp(int $timestamp): self
+    {
+        return new self(date(DateTime::DATETIME, $timestamp));
+    }
+
+    /**
      * @return string
      */
     public function __toString()
