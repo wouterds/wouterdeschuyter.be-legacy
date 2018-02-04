4639ebc745562e3b6515546a9c34321c6b31b34d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-02-04 16:13:25 +0100

    :sparkles: Fetch, post process & pass to view the chart data

--- src/Application/Http/Handlers/StatsHandler.php
+++ src/Application/Http/Handlers/StatsHandler.php
@@ -2,6 +2,9 @@
 
 namespace WouterDeSchuyter\Application\Http\Handlers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+use WouterDeSchuyter\Domain\AccessLogs\AccessLogRepository;
 use WouterDeSchuyter\Infrastructure\View\ViewAwareInterface;
 use WouterDeSchuyter\Infrastructure\View\ViewAwareTrait;
 
@@ -10,10 +13,104 @@ class StatsHandler implements ViewAwareInterface
     use ViewAwareTrait;
 
     /**
+     * @var AccessLogRepository
+     */
+    private $accessLogRepository;
+
+    /**
+     * @param AccessLogRepository $accessLogRepository
+     */
+    public function __construct(AccessLogRepository $accessLogRepository)
+    {
+        $this->accessLogRepository = $accessLogRepository;
+    }
+
+    /**
      * @return string
      */
     public function getTemplate(): string
     {
         return 'pages/stats.html.twig';
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $responseCodesPerHourLastDay = $this->responseCodesPerHourLastDay();
+
+        $data = [];
+        $data['responseCodesPerHourLastDay'] = $responseCodesPerHourLastDay;
+
+        return $this->render($response, $data);
+    }
+
+    /**
+     * @return array
+     */
+    private function responseCodesPerHourLastDay(): array
+    {
+        $responseCodesPerHourLastDay = $this->accessLogRepository->responseCodesPerHourLastDay();
+
+        $responseCodesPerHourLastDayTmp = [];
+        foreach ($responseCodesPerHourLastDay as $data) {
+            if (!isset($responseCodesPerHourLastDayTmp[$data['hour']])) {
+                $responseCodesPerHourLastDayTmp[$data['hour']] = [];
+            }
+
+            $responseCodesPerHourLastDayTmp[$data['hour']][] = [
+                'status_code' => $data['status_code'],
+                'count' => $data['count']
+            ];
+        }
+
+        $responseCodesPerHourLastDay = [];
+        for ($i = 0; $i <= 23; $i++) {
+            $hour = $i;
+            if (strlen($hour) === 1) {
+                $hour = '0' . $hour;
+            }
+
+            $data = isset($responseCodesPerHourLastDayTmp[$hour]) ? $responseCodesPerHourLastDayTmp[$hour] : null;
+            $hour = $hour . ':00';
+
+            if (!isset($responseCodesPerHourLastDay[$hour])) {
+                $responseCodesPerHourLastDay[$hour] = [];
+            }
+
+            if (!$data) {
+                continue;
+            }
+
+            foreach ($data as $item) {
+                $status = $item['status_code'];
+                if (in_array($status, [200, 304])) {
+                    $status = '200304';
+                } elseif (in_array($status, [301, 302])) {
+                    $status = '301302';
+                } elseif (in_array($status, [400, 401, 403, 404])) {
+                    $status = '400401403404';
+                } elseif ($status >= 500) {
+                    $status = '50x';
+                }
+
+                $responseCodesPerHourLastDay[$hour][$status] = $item['count'];
+            }
+        }
+
+        $responseCodesPerHourLastDayTmp = [];
+        $hour = date('H') . ':00';
+        $i = 1;
+        while (count($responseCodesPerHourLastDayTmp) < 24) {
+            $data = $responseCodesPerHourLastDay[$hour];
+            $data['hour'] = $hour;
+            $responseCodesPerHourLastDayTmp[] = $data;
+            $hour = date('H', strtotime('-' . $i++ . ' hour')) . ':00';
+        }
+
+        return array_reverse($responseCodesPerHourLastDayTmp);
     }
 }
