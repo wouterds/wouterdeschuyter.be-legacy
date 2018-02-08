1b979942b6c149696ea4b71bfd20754addb1c56d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-02-08 01:17:09 +0100

    :zap: Home controller is actually same as blog page but with different template, but same components

--- src/Application/Http/Handlers/HomeHandler.php
+++ src/Application/Http/Handlers/HomeHandler.php
@@ -2,29 +2,11 @@
 
 namespace WouterDeSchuyter\Application\Http\Handlers;
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-use WouterDeSchuyter\Domain\Blog\BlogPostRepository;
+use WouterDeSchuyter\Application\Http\Handlers\Blog\IndexHandler as BlogIndexHandler;
 use WouterDeSchuyter\Infrastructure\View\ViewAwareInterface;
-use WouterDeSchuyter\Infrastructure\View\ViewAwareTrait;
 
-class HomeHandler implements ViewAwareInterface
+class HomeHandler extends BlogIndexHandler implements ViewAwareInterface
 {
-    use ViewAwareTrait;
-
-    /**
-     * @var BlogPostRepository
-     */
-    private $blogPostRepository;
-
-    /**
-     * @param BlogPostRepository $blogPostRepository
-     */
-    public function __construct(BlogPostRepository $blogPostRepository)
-    {
-        $this->blogPostRepository = $blogPostRepository;
-    }
-
     /**
      * @return null|string
      */
@@ -39,20 +21,5 @@ class HomeHandler implements ViewAwareInterface
     public function getTemplate(): string
     {
         return 'pages/home.html.twig';
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     */
-    public function __invoke(Request $request, Response $response): Response
-    {
-        $blogPosts = $this->blogPostRepository->findPublished(0, 7);
-
-        $data = [];
-        $data['blogPosts'] = $blogPosts;
-
-        return $this->render($response, $data);
     }
 }
