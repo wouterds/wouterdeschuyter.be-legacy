2fd0863163c7432debcccd933fe04731299ffe39
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-02-11 21:42:06 +0100

    :sparkles: Added ChangeRatio command

---
+++ src/Domain/Commands/Media/ChangeRatio.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace WouterDeSchuyter\Domain\Commands\Media;
+
+use WouterDeSchuyter\Domain\Media\MediaId;
+
+class ChangeRatio
+{
+    /**
+     * @var MediaId
+     */
+    private $mediaId;
+
+    /**
+     * @var float
+     */
+    private $ratio;
+
+    /**
+     * @var int
+     */
+    private $maxWidth;
+
+    /**
+     * @param MediaId $mediaId
+     * @param float $ratio
+     * @param int $maxWidth
+     */
+    public function __construct(MediaId $mediaId, float $ratio, int $maxWidth)
+    {
+        $this->mediaId = $mediaId;
+        $this->ratio = $ratio;
+        $this->maxWidth = $maxWidth;
+    }
+
+    /**
+     * @return MediaId
+     */
+    public function getMediaId(): MediaId
+    {
+        return $this->mediaId;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRatio(): float
+    {
+        return $this->ratio;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxWidth(): int
+    {
+        return $this->maxWidth;
+    }
+}
