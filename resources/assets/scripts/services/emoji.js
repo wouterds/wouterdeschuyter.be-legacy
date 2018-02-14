48d8319587e40edbc3af10da3e4828fa12aa81a4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-02-14 18:12:25 +0100

    :art: Fixed indenting

--- resources/assets/scripts/services/emoji.js
+++ resources/assets/scripts/services/emoji.js
@@ -3,19 +3,19 @@ import twemoji from 'twemoji';
 class Emoji {
   constructor() {
     twemoji.parse(document.body.getElementsByTagName('main')[0], {
-        folder: 'svg',
-        ext: '.svg',
-        callback: function (icon, options) {
-          switch (icon) {
-            case 'a9': // © copyright
-            case 'ae': // ® registered trademark
-            case '2122': // ™ trademark
-              return false;
-          }
-
-          return ''.concat(options.base, options.size, '/', icon, options.ext);
+      folder: 'svg',
+      ext: '.svg',
+      callback: function (icon, options) {
+        switch (icon) {
+          case 'a9': // © copyright
+          case 'ae': // ® registered trademark
+          case '2122': // ™ trademark
+          return false;
         }
-      });
+
+        return ''.concat(options.base, options.size, '/', icon, options.ext);
+      },
+    });
   }
 }
 
