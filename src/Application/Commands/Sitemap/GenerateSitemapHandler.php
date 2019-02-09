614dfda0a72de4dff6d562876950b49c6d832086
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-02-09 14:11:28 +0100

    :rotating_light: Linting™

--- src/Application/Commands/Sitemap/GenerateSitemapHandler.php
+++ src/Application/Commands/Sitemap/GenerateSitemapHandler.php
@@ -77,12 +77,6 @@ class GenerateSitemapHandler
             Sitemap::DAILY,
             0.9
         );
-        // $sitemap->addItem(
-        //     $this->config->get('APP_URL') . $this->router->pathFor('projects'),
-        //     time(),
-        //     Sitemap::MONTHLY,
-        //     0.9
-        // );
         $sitemap->addItem(
             $this->config->get('APP_URL') . $this->router->pathFor('contact'),
             time(),
